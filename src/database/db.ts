6b050cad69df82d89a8ab1324d9a7e70800a5e9a
protoss70 <tlegendgaming@gmail.com>
2024-10-30 15:15:23 +0100

    connected to db

---
+++ src/database/db.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("Mongo URI is not defined");
+    }
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectDB;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import connectDB from './database/db'; // Import the connectDB function
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
 
 // Use authentication routes
 app.use('/api/auth', authRoutes);
