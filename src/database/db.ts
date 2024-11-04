3f8960ec2bd65ed5e39586e52489cecb9f6ed88c
protoss70 <tlegendgaming@gmail.com>
2024-11-04 17:14:06 +0100

    console log update

--- src/database/db.ts
+++ src/database/db.ts
@@ -9,7 +9,7 @@ const connectDB = async () => {
       throw new Error("Mongo URI is not defined");
     }
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected successfully');
+    console.log('MongoDB connected successfully!');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1); // Exit process with failure
